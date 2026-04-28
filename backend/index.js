4a56bf4b60704eaf593d8106e8147e533639c776
Srashti Chauhan <srashtichauhan24@gmail.com>
2026-04-29 01:50:31 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,21 @@ app.use(cookieParser())
 //     origin:'https://localhost:5173',
 //     credentials:true
 // }))
+// app.use(cors({
+//   // origin: ["http://localhost:5173"],
+//    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+//   credentials: true
+// }));
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173"
+].filter(Boolean);
+
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: allowedOrigins,
   credentials: true
 }));
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
 
 app.use('/api/auth', authRoute)
